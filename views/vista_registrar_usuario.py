b60a3e635e83dd0cfc13fd5576b128c4e89e2288
AlvaZzz <165922749+AlvaZzz@users.noreply.github.com>
2024-06-29 13:30:11 -0400

    XD
    XD

--- views/vista_registrar_usuario.py
+++ views/vista_registrar_usuario.py
@@ -32,38 +32,38 @@ class V_RegistrarUsuario:
         self.frame.place(relx=0.6, rely=0.5, anchor=tk.CENTER)
         
         # Etiquetas y widgets de entrada
-        self.lb_nombre = tk.Label(self.frame, text="Nombre: ", bg="#e6e6e6", fg="#225e68", font=("Arial", 12, "bold"))
+        self.lb_nombre = tk.Label(self.frame, text="Nombre: ", bg="#e6e6e6", fg="#225e68", font=("Arial", 10, "bold"))
         self.lb_nombre.grid(row=1,column=0)
-        self.entrada_nombre = tk.Entry(self.frame,font=("Arial", 12, "bold"))
+        self.entrada_nombre = tk.Entry(self.frame,font=("Arial", 10, "bold"))
         self.entrada_nombre.grid(row=2,column=0,columnspan=2,sticky="nesw",padx=(20,20))
         
-        self.lb_stock = tk.Label(self.frame, text="Edad: ", bg="#e6e6e6", fg="#225e68", font=("Arial", 12, "bold"))
+        self.lb_stock = tk.Label(self.frame, text="Edad: ", bg="#e6e6e6", fg="#225e68", font=("Arial", 10, "bold"))
         self.lb_stock.grid(row=3,column=0)
-        self.entrada_stock = tk.Entry(self.frame,font=("Arial", 12, "bold"))
+        self.entrada_stock = tk.Entry(self.frame,font=("Arial", 10, "bold"))
         self.entrada_stock.grid(row=4,column=0,padx=(20,20))
     
-        self.lb_ci = tk.Label(self.frame, text="CI: ", bg="#e6e6e6", fg="#225e68", font=("Arial", 12, "bold"))
+        self.lb_ci = tk.Label(self.frame, text="CI: ", bg="#e6e6e6", fg="#225e68", font=("Arial", 10, "bold"))
         self.lb_ci.grid(row=3,column=1)
-        self.entrada_ci = tk.Entry(self.frame,font=("Arial", 12, "bold"))
+        self.entrada_ci = tk.Entry(self.frame,font=("Arial", 10, "bold"))
         self.entrada_ci.grid(row=4,column=1,padx=(0,20))
         
-        self.lb_usuario = tk.Label(self.frame, text="Usuario: ", bg="#e6e6e6", fg="#225e68", font=("Arial", 12, "bold"))
+        self.lb_usuario = tk.Label(self.frame, text="Usuario: ", bg="#e6e6e6", fg="#225e68", font=("Arial", 10, "bold"))
         self.lb_usuario.grid(row=5, column=0)
-        self.entrada_usuario = tk.Entry(self.frame,font=("Arial", 12, "bold"))
+        self.entrada_usuario = tk.Entry(self.frame,font=("Arial", 10, "bold"))
         self.entrada_usuario.grid(row=6, column=0,padx=(20,20))
         
-        self.lb_password = tk.Label(self.frame, text="Password: ", bg="#e6e6e6", fg="#225e68", font=("Arial", 12, "bold"))
+        self.lb_password = tk.Label(self.frame, text="Password: ", bg="#e6e6e6", fg="#225e68", font=("Arial", 10, "bold"))
         self.lb_password.grid(row=5,column=1)
-        self.entrada_password = tk.Entry(self.frame,font=("Arial", 12, "bold"))
+        self.entrada_password = tk.Entry(self.frame,font=("Arial", 10, "bold"))
         self.entrada_password.grid(row=6,column=1,padx=(0,20))
         
-        self.lb_confirmar = tk.Label(self.frame, text="Confirmar: ", bg="#e6e6e6", fg="#225e68", font=("Arial", 12, "bold"))
+        self.lb_confirmar = tk.Label(self.frame, text="Confirmar: ", bg="#e6e6e6", fg="#225e68", font=("Arial", 10, "bold"))
         self.lb_confirmar.grid(row=7,column=1)
-        self.entrada_confirmar = tk.Entry(self.frame,font=("Arial", 12, "bold"))
+        self.entrada_confirmar = tk.Entry(self.frame,font=("Arial", 10, "bold"))
         self.entrada_confirmar.grid(row=8,column=1,padx=(0,20))
         
         # Botón
-        self.btn_registrar = tk.Button(self.frame, text="Registrar", bg="#225e68", fg="white", font=("Arial", 12, "bold"))
+        self.btn_registrar = tk.Button(self.frame, text="Registrar", bg="#225e68", fg="white", font=("Arial", 10, "bold"))
         self.btn_registrar.grid(row=9,column=1,pady=(20,5))
         
         
