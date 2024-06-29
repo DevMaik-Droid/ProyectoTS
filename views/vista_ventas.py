92e17f27bf5056b891d5fd4e439052e5952cf4d2
AlvaZzz <165922749+AlvaZzz@users.noreply.github.com>
2024-06-29 11:02:12 -0400

    xd
    xd

--- views/vista_ventas.py
+++ views/vista_ventas.py
@@ -3,68 +3,63 @@ from tkinter import ttk
 from PIL import Image, ImageTk
 from services.Service_Producto import listar_productos
 
-# Supongamos que tenemos una función para listar productos
-# Aquí se define una función de ejemplo
-
 class V_Ventas:
     
     def __init__(self, ventana):
         self.ventana = ventana
         self.ventana.title("Registro de Venta")
-        self.ventana.geometry("750x500")  # Se ha aumentado el ancho de la ventana
+        self.ventana.geometry("750x500") 
         self.crear_fondo()
         self.init_components()
 
     def crear_fondo(self):
-    # Cargar la imagen de fondo
         self.background_image = Image.open("images/fondo_ventas.jpg")
         self.background_photo = ImageTk.PhotoImage(self.background_image)
 
-        # Crear una etiqueta para mostrar la imagen de fondo
         self.background_label = tk.Label(self.ventana, image=self.background_photo)
         self.background_label.place(x=0, y=0, relwidth=1, relheight=1)
 
     def init_components(self):
-        self.lb_titulo = tk.Label(self.ventana, text="Registro de Venta", fg="#2C3E50", font=("Arial", 20, "bold"))
+        self.lb_titulo = tk.Label(self.ventana, text="Registro de Venta", fg="white",bg = "#0f0700", font=("Arial", 20, "bold"))
         self.lb_titulo.grid(row=0, column=1, pady=(20, 10))
 
-        self.lb_nombre_cliente = tk.Label(self.ventana, text="Cliente:", fg="#2C3E50")
+        self.lb_nombre_cliente = tk.Label(self.ventana, text="Cliente:", fg="white",bg = "#0f0700",font=("Arial", 12, "bold"))
         self.lb_nombre_cliente.grid(row=1, column=0,padx=(10,0))
         self.entrada_nombre_cliente = tk.Entry(self.ventana)
         self.entrada_nombre_cliente.grid(row=2, column=0, padx=(10,0))
         
-        self.lb_ci_cliente = tk.Label(self.ventana, text="Cédula:", bg="#E5E8E8", fg="#2C3E50")
+        self.lb_ci_cliente = tk.Label(self.ventana, text="Cédula:", fg="white",bg = "#0f0700",font=("Arial", 12, "bold"))
         self.lb_ci_cliente.grid(row=1, column=1, padx=10)
         self.entrada_ci_cliente = tk.Entry(self.ventana)        
         self.entrada_ci_cliente.grid(row=2, column=1, padx=10)
         
-        self.lb_nombre_producto = tk.Label(self.ventana, text="Producto:", bg="#E5E8E8", fg="#2C3E50")
+        self.lb_nombre_producto = tk.Label(self.ventana, text="Producto:", fg="white",bg = "#0f0700",font=("Arial", 12, "bold"))
         self.lb_nombre_producto.grid(row=3, column=0, padx=(10,0), pady=(10,0))
         self.entrada_nombre_producto = tk.Entry(self.ventana)
         self.entrada_nombre_producto.grid(row=4, column=0, padx=(10,0), pady=(10,0))
         
-        self.lb_precio = tk.Label(self.ventana, text="Precio:", bg="#E5E8E8", fg="#2C3E50")
+        self.lb_precio = tk.Label(self.ventana, text="Precio:", fg="white",bg = "#0f0700",font=("Arial", 12, "bold"))
         self.lb_precio.grid(row=3, column=1, padx=10, pady=(10,0))
         self.entrada_precio = tk.Entry(self.ventana)
         self.entrada_precio.grid(row=4, column=1, padx=10, pady=(10,0))
         
-        self.lb_total = tk.Label(self.ventana, text="Total:", bg="#E5E8E8", fg="#2C3E50")
-        self.lb_total.grid(row=3, column=2, padx=10, pady=(10,0))  # Alineado a la derecha con respecto al precio
+        self.lb_total = tk.Label(self.ventana, text="Total:", fg="white",bg = "#0f0700",font=("Arial", 12, "bold"))
+        self.lb_total.grid(row=3, column=2, padx=10, pady=(10,0))  
         self.entrada_total = tk.Entry(self.ventana)
         self.entrada_total.grid(row=4, column=2, padx=10, pady=(10,0))
         
-        self.lb_cantidad = tk.Label(self.ventana, text="Cantidad:", bg="#E5E8E8", fg="#2C3E50")
-        self.lb_cantidad.grid(row=1, column=2, padx=10, pady=(10,0))  # Alineado a la derecha con respecto a la cédula
-        self.entrada_cantidad = tk.Entry(self.ventana)  # Se reduce el ancho del campo de cantidad
+        self.lb_cantidad = tk.Label(self.ventana, text="Cantidad:", fg="white",bg = "#0f0700",font=("Arial", 12, "bold"))
+        self.lb_cantidad.grid(row=1, column=2, padx=10, pady=(10,0))  
+        self.entrada_cantidad = tk.Entry(self.ventana) 
         self.entrada_cantidad.grid(row=2, column=2, padx=10, pady=(10,0))
 
-        self.btn_vender = tk.Button(self.ventana, text="Vender", bg="#2C3E50", fg="white", font=("Arial", 12, "bold"))
+        self.btn_vender = tk.Button(self.ventana, text="Vender", fg="white",bg = "#74512D",font=("Arial", 12, "bold"))
         self.btn_vender.grid(row=5, column=1, pady=10, padx=10)
 
-        self.lb_clientes = tk.Label(self.ventana, text="Clientes:")
+        self.lb_clientes = tk.Label(self.ventana, text="Clientes:", fg="white",bg = "#0f0700",font=("Arial", 12, "bold"))
         self.lb_clientes.grid(row=6, column=0)
         
-        self.lb_productos = tk.Label(self.ventana, text="Productos:")
+        self.lb_productos = tk.Label(self.ventana, text="Productos:", fg="white",bg = "#0f0700",font=("Arial", 12, "bold"))
         self.lb_productos.grid(row=6, column=1, sticky='w', padx=10)
         
         self.generar_tabla()
@@ -82,14 +77,14 @@ class V_Ventas:
         self.tabla_productos.heading("Libro", text="Libro")
         self.tabla_productos.heading("Stock", text="Stock")
         self.tabla_productos.heading("Precio", text="Precio")
-        self.tabla_productos.grid(row=7, column=1, columnspan=2, sticky='nsew', pady=(10, 20), padx=10)  # Ajuste de la tabla más arriba y un poco más abajo
+        self.tabla_productos.grid(row=7, column=1, columnspan=2, sticky='nsew', pady=(10, 20), padx=10)  
 
         list_productos = listar_productos()
         
         for producto in list_productos:
             self.tabla_productos.insert("", tk.END, values=producto)
             
-        self.tabla_productos.bind('<ButtonRelease-1>', self.evento_tabla) # Da evento a la tabla productos
+        self.tabla_productos.bind('<ButtonRelease-1>', self.evento_tabla) 
     
     def generar_tabla_cliente(self):
         columnas = ("ID Cliente", "Cliente", "CI")
